d24b717ffd1c2f446f2881e8858e3c47f81ddcaa
Hannah (Orion) Dobos <56137148+gobos12@users.noreply.github.com>
2022-03-22 11:44:35 -0700

    making the start menu scene
    yo dawg i heard u like cars so i put a car in your car so you can drive while you drive

---
+++ MemeJam/Assets/Scripts/UIScripts/SceneSwitcher.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void GoToMainScene(){
+        SceneManager.LoadScene("MainScene");
+    }
+    
+    public void GoToControlsScene(){
+        SceneManager.LoadScene("ControlsScene");
+    }
+}
