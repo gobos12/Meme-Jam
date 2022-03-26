ea2c9b41cd7d3f0b7c98d9e8535f12b5dca7fc85
Summer Gautier <summergautier@gmail.com>
2022-03-26 15:26:02 -0700

    finalized camera angle & fixed rotation speed glitch

--- MemeJam/Assets/Scripts/Car/CameraFollow.cs
+++ MemeJam/Assets/Scripts/Car/CameraFollow.cs
@@ -24,16 +24,6 @@ public class CameraFollow : MonoBehaviour
     private void HandleRotation(){
         var direction = target.position - transform.position;
         var targetRotation = Quaternion.LookRotation(direction, Vector3.up);
-        transform.rotation = SmoothDampQuaternion(transform.rotation, targetRotation, ref velocity, rotationSpeed * Time.deltaTime);
-    }
-
-    public static Quaternion SmoothDampQuaternion(Quaternion current, Quaternion target, ref Vector3 currentVelocity, float smoothTime){
-        Vector3 c = current.eulerAngles;
-        Vector3 t = target.eulerAngles;
-        return Quaternion.Euler(
-            Mathf.SmoothDampAngle(c.x, t.x, ref currentVelocity.x, smoothTime),
-            Mathf.SmoothDampAngle(c.y, t.y, ref currentVelocity.y, smoothTime),
-            Mathf.SmoothDampAngle(c.z, t.z, ref currentVelocity.z, smoothTime)
-        );
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
     }
 }
